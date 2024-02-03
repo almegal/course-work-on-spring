808df1edf626065a84ada9376e722088f67b00d1
alekseipeikhel <alekseipeikhel@gmail.com>
2024-02-03 17:54:38 +0300

    initial commit

---
+++ src/main/java/com/skyhomework/courseworkonspring/CourseWorkOnSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skyhomework.courseworkonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkOnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkOnSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skyhomework/courseworkonspring/CourseWorkOnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyhomework.courseworkonspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkOnSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
