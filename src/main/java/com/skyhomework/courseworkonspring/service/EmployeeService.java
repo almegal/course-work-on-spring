7dcd2ab54274e98261cbc9599c8f65fb4a0ede06
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-06 01:22:36 +0300

    added org.apache.commons.lang3.StringUtils; realized validation

--- src/main/java/com/skyhomework/courseworkonspring/service/EmployeeService.java
+++ src/main/java/com/skyhomework/courseworkonspring/service/EmployeeService.java
@@ -1,9 +1,11 @@
 package com.skyhomework.courseworkonspring.service;
 
+import com.skyhomework.courseworkonspring.Exception.ValidateException;
 import com.skyhomework.courseworkonspring.model.Employee;
 import com.skyhomework.courseworkonspring.Exception.EmployeeAlreadyAddedException;
 import com.skyhomework.courseworkonspring.Exception.EmployeeNotFoundException;
 import com.skyhomework.courseworkonspring.Exception.EmployeeStorageIsFullException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,7 +21,7 @@ public class EmployeeService  {
     @ResponseStatus(code = HttpStatus.OK)
     public Employee add(String f, String l, int d, int s)
             throws EmployeeAlreadyAddedException, EmployeeStorageIsFullException {
-
+        validateData(f, l);
         Employee empl = new Employee(f, l, d, s);
 
         if(employeeBook.size() >= maxEmployeesInCompany) throw new EmployeeStorageIsFullException("max limited employees in company achive (" + maxEmployeesInCompany +")");
@@ -31,7 +33,7 @@ public class EmployeeService  {
     @ResponseStatus(code = HttpStatus.OK)
     public Employee remove(String f, String l)
             throws EmployeeNotFoundException {
-
+        validateData(f, l);
         String key = f + " " + l;
 
         if(employeeBook.containsKey(key)) throw new EmployeeNotFoundException("Employee not found in collection");
@@ -40,6 +42,7 @@ public class EmployeeService  {
     }
     @ResponseStatus(code = HttpStatus.OK)
     public Employee find(String f, String l) throws EmployeeNotFoundException {
+        validateData(f, l);
         String key = f + " " + l;
         if(!employeeBook.containsKey(key)) throw new EmployeeNotFoundException("Employee not found in collection");
         return employeeBook.get(key);
@@ -50,4 +53,25 @@ public class EmployeeService  {
         return employeeBook;
     }
 
+    private void validateData(String f, String l) {
+        String capitalizeFirstName = StringUtils.capitalize(f);
+        String capitalizeLastName = StringUtils.capitalize(l);
+        boolean isFirstNameAlpha = StringUtils.isAlpha(f);
+        boolean isLastNameAlpha = StringUtils.isAlpha(l);
+
+        if(
+                !f.equals(capitalizeFirstName) ||
+                !l.equals(capitalizeLastName)
+        ) {
+            throw new ValidateException("Имя или Фамилия не с большой буквы");
+        }
+
+        if(
+                !isFirstNameAlpha ||
+                !isLastNameAlpha
+        ){
+            throw new ValidateException("Имя или Фамилия содержит запрещенные символы");
+        }
+
+    }
 }
