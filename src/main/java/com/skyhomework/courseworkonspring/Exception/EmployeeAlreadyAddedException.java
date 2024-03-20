a2df751220ed59f36ae33fe1c75124b89f4b27b4
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-20 10:48:24 +0300

    change type exception to RunTime

--- src/main/java/com/skyhomework/courseworkonspring/Exception/EmployeeAlreadyAddedException.java
+++ src/main/java/com/skyhomework/courseworkonspring/Exception/EmployeeAlreadyAddedException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class EmployeeAlreadyAddedException extends Exception {
+public class EmployeeAlreadyAddedException extends RuntimeException {
     public EmployeeAlreadyAddedException(String message) {
         super(message);
     }
--- src/main/java/com/skyhomework/courseworkonspring/Exception/EmployeeNotFoundException.java
+++ src/main/java/com/skyhomework/courseworkonspring/Exception/EmployeeNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class EmployeeNotFoundException extends Exception {
+public class EmployeeNotFoundException extends RuntimeException {
     public EmployeeNotFoundException(String message) {
         super(message);
     }
