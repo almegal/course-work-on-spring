8ce825de1ae07da39287d61f2ac292c36837317f
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-20 10:49:26 +0300

    delete try catch block

--- src/main/java/com/skyhomework/courseworkonspring/controller/EmployeeController.java
+++ src/main/java/com/skyhomework/courseworkonspring/controller/EmployeeController.java
@@ -1,7 +1,7 @@
 package com.skyhomework.courseworkonspring.controller;
 
 import com.skyhomework.courseworkonspring.model.Employee;
-import com.skyhomework.courseworkonspring.service.EmployeeService;
+import com.skyhomework.courseworkonspring.service.EmployeeServiceImpl;
 import com.skyhomework.courseworkonspring.Exception.EmployeeAlreadyAddedException;
 import com.skyhomework.courseworkonspring.Exception.EmployeeNotFoundException;
 import com.skyhomework.courseworkonspring.Exception.EmployeeStorageIsFullException;
@@ -17,9 +17,9 @@ import java.util.Map;
 @RestController
 @RequestMapping("employee/")
 public class EmployeeController {
-    public final EmployeeService emplService;
+    public final EmployeeServiceImpl emplService;
 
-    public EmployeeController(EmployeeService emplService) {
+    public EmployeeController(EmployeeServiceImpl emplService) {
         this.emplService = emplService;
     }
 
@@ -28,25 +28,13 @@ public class EmployeeController {
                         @RequestParam("lastName") String l,
                         @RequestParam("dpt") int d,
                         @RequestParam("slr") int s
-    ) {
-        Employee empl;
-        try {
-            empl = emplService.add(f, l, d, s);
-        } catch (EmployeeStorageIsFullException | EmployeeAlreadyAddedException e) {
-            throw new RuntimeException(e);
-        }
-        return empl;
+    ){
+        return emplService.add(f, l, d, s);
     }
     @GetMapping(path="/remove", produces = MediaType.APPLICATION_JSON_VALUE)
     public Employee remove(@RequestParam("firstName") String f,
                            @RequestParam("lastName") String l){
-        Employee empl;
-        try {
-            empl = emplService.remove(f, l);
-        } catch (EmployeeNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        return empl;
+        return emplService.remove(f, l);
     }
     @GetMapping(path="/find", produces = MediaType.APPLICATION_JSON_VALUE)
     public Employee find(@RequestParam("firstName") String f,
