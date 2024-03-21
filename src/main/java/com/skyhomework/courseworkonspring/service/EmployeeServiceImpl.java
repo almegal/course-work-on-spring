ecd36a88cd09caa3a79ac39ef4ffc22f927135a4
alekseipeikhel <alekseipeikhel@gmail.com>
2024-03-21 14:53:22 +0300

    added ew method to validate, change error msg

--- src/main/java/com/skyhomework/courseworkonspring/service/EmployeeServiceImpl.java
+++ src/main/java/com/skyhomework/courseworkonspring/service/EmployeeServiceImpl.java
@@ -1,6 +1,5 @@
 package com.skyhomework.courseworkonspring.service;
 
-import com.skyhomework.courseworkonspring.Exception.ValidateException;
 import com.skyhomework.courseworkonspring.model.Employee;
 import com.skyhomework.courseworkonspring.Exception.EmployeeAlreadyAddedException;
 import com.skyhomework.courseworkonspring.Exception.EmployeeNotFoundException;
@@ -15,63 +14,81 @@ import java.util.Map;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final int maxEmployeesInCompany = 10;
+    //Допустимое количество сотрудников в компании по умолчанию
+    private int maxEmployeesInCompany = 10;
     private final Map<String, Employee> employeeBook = new HashMap<>();
-
+    // метод добавления
     @Override
     @ResponseStatus(code = HttpStatus.OK)
     public Employee add(String f, String l, int d, int s) {
-        validateData(f, l);
-        Employee empl = new Employee(f, l, d, s);
-
-        if(employeeBook.size() >= maxEmployeesInCompany) throw new EmployeeStorageIsFullException("max limited employees in company achive (" + maxEmployeesInCompany +")");
-        if(employeeBook.containsValue(empl)) throw new EmployeeAlreadyAddedException("Employee is exsist/added in storage");
-
+        //проверяем что переданные значения валидные
+        validateParams(f, l);
+        // делаем строки строки с Заглавной буквы
+        String validName = changeStringToCapitalize(f);
+        String validLastName = changeStringToCapitalize(l);
+        // создаем экземпляр работника
+        Employee empl = new Employee(validName, validLastName, d, s);
+        // если размен книги больше допустимого значения вернуть ошибку
+        if(employeeBook.size() >= maxEmployeesInCompany) throw new EmployeeStorageIsFullException("Нельзя больше добавить сотрудников, превышен лимит работников");
+        // если значение уже есть в списке вернуть ошибку
+        if(employeeBook.containsValue(empl)) throw new EmployeeAlreadyAddedException("Такой сотрудник уже есть в списке");
+        // добавляем работника в словарь
         employeeBook.put(empl.getFullName(), empl);
         return empl;
     }
+    // метод удаления
     @Override
     @ResponseStatus(code = HttpStatus.OK)
     public Employee remove(String f, String l) {
-        validateData(f, l);
-        String key = f + " " + l;
-        if(!employeeBook.containsKey(key)) throw new EmployeeNotFoundException("Employee not found in collection");
-
-        return employeeBook.get(key);
+        //проверяем что переданные значения валидные
+        validateParams(f, l);
+        // делаем строки строки с Заглавной буквы
+        String validName = changeStringToCapitalize(f);
+        String validLastName = changeStringToCapitalize(l);
+        // создаем ключ для поиска в словаре
+        String key = validName.concat(" ").concat(validLastName);
+        // если такого работника нет в словаре по ключу - вернуть ошибку
+        if(!employeeBook.containsKey(key)) throw new EmployeeNotFoundException("Такого работника не существует");
+        return employeeBook.remove(key);
     }
+    // метод поиска
     @Override
     @ResponseStatus(code = HttpStatus.OK)
     public Employee find(String f, String l){
-        validateData(f, l);
-        String key = f + " " + l;
-        if(!employeeBook.containsKey(key)) throw new EmployeeNotFoundException("Employee not found in collection");
+        //проверяем что переданные значения валидные
+        validateParams(f, l);
+        // делаем строки строки с Заглавной буквы
+        String validName = changeStringToCapitalize(f);
+        String validLastName = changeStringToCapitalize(l);
+        // создаем ключ для поиска в словаре
+        String key = validName.concat(" ").concat(validLastName);
+        // если такого работника нет в словаре по ключу - вернуть ошибку
+        if(!employeeBook.containsKey(key)) throw new EmployeeNotFoundException("Такого работника не существует");
         return employeeBook.get(key);
     }
-
+    // вспомогательный метод для приведения строки к формату Sssss
+    private String changeStringToCapitalize(String str) {
+        // привеодим значение к нижнему регистру
+        String lowetStr = str.toLowerCase();
+        // используем метод библиотеки string.utils чтобы вернуть верную строку
+        return StringUtils.capitalize(lowetStr);
+    }
+    // вспомогателньый метод для проверки корректности параметров
+    private void validateParams(String f, String l) {
+        // метод isAlpha срабатывает если в строке есть цифры
+        // а также на ' ',  '', null,
+        boolean lNameIsAlpha = StringUtils.isAlpha(l);
+        boolean fNameIsAlpha = StringUtils.isAlpha(f);
+        // если содержит такие символы - вернуть ошибку
+        if(!fNameIsAlpha || !lNameIsAlpha) {
+            throw new IllegalArgumentException("Переданы некорректные аргументы");
+        }
+    }
     @ResponseStatus(code = HttpStatus.OK)
     public Map<String, Employee> getEmployee() {
         return employeeBook;
     }
-
-    private void validateData(String f, String l) {
-        String capitalizeFirstName = StringUtils.capitalize(f);
-        String capitalizeLastName = StringUtils.capitalize(l);
-        boolean isFirstNameAlpha = StringUtils.isAlpha(f);
-        boolean isLastNameAlpha = StringUtils.isAlpha(l);
-
-        if(
-                !f.equals(capitalizeFirstName) ||
-                !l.equals(capitalizeLastName)
-        ) {
-            throw new ValidateException("Имя или Фамилия не с большой буквы");
-        }
-
-        if(
-                !isFirstNameAlpha ||
-                !isLastNameAlpha
-        ){
-            throw new ValidateException("Имя или Фамилия содержит запрещенные символы");
-        }
-
+    public void setMaxEmployeesInCompany(int maxEmployeesInCompany) {
+        this.maxEmployeesInCompany = maxEmployeesInCompany;
     }
 }
